ccec579b1f172b7dfd9fb38a285f286eb62c6eec
Sean Gomes-Luis <seang4@discovery.co.za>
2023-05-16 11:55:36 +0200

    MainView updates

--- SwiftUIBitesize/Views/MainView.swift
+++ SwiftUIBitesize/Views/MainView.swift
@@ -5,39 +5,35 @@ struct MainView: View {
 
     // MARK: - Body
     var body: some View {
-        ZStack {
-            NavigationView {
-                VStack(alignment: .center, spacing: 0) {
-                    SportsView(viewModel: viewModel)
-                    Spacer()
-                    VStack(alignment: .center, spacing: 12) {
-                        Text(viewModel.selectedSport?.title.uppercased() ?? unselectedSportDescription)
-                            .foregroundColor(Color.gray)
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .shadow(radius: 1)
-                            .padding(.horizontal, 18)
-                            .padding(.vertical, 4)
-                            .overlay(Rectangle()
-                                .fill(Color.black)
-                                .frame(height: 1), alignment: .bottom)
-                            .animation(.default,
-                                       value: viewModel.selectedSport?.title.uppercased() ?? unselectedSportDescription)
-                        Text(selectSportDescription.uppercased())
-                            .foregroundColor(Color.black)
-                            .font(.footnote)
-                            .fontWeight(.bold)
-                            .frame(minWidth: 85)
-                            .padding(.horizontal, 10)
-                            .padding(.vertical, 5)
-                            .background(Capsule().fill(Color.white))
-                    } //: VStack
-                    .frame(minWidth: 280)
+        NavigationView {
+            VStack(alignment: .center, spacing: 0) {
+                SportsView(viewModel: viewModel)
+                VStack(alignment: .center, spacing: 12) {
+                    Text(viewModel.selectedSport?.title.uppercased() ?? unselectedSportDescription)
+                        .foregroundColor(Color.gray)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .shadow(radius: 1)
+                        .padding(.horizontal, 18)
+                        .padding(.vertical, 4)
+                        .overlay(Rectangle()
+                            .fill(Color.black)
+                            .frame(height: 1), alignment: .bottom)
+                        .animation(.easeInOut, value: viewModel.selectedSport?.title.uppercased() ?? unselectedSportDescription)
+                    Text(selectSportDescription.uppercased())
+                        .foregroundColor(Color.black)
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .frame(minWidth: 85)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 5)
+                        .background(Capsule().fill(Color.white))
                 } //: VStack
-                .navigationTitle(navigationViewTitle)
-                .navigationBarTitleDisplayMode(.inline)
-            } //: NAVIGATION
-        } //: ZStack
+                .frame(minWidth: 280)
+            } //: VStack
+            .navigationTitle(navigationViewTitle)
+            .navigationBarTitleDisplayMode(.inline)
+        } //: NAVIGATION
     }
 }
 
@@ -54,19 +50,3 @@ struct MainView_Previews: PreviewProvider {
         MainView(viewModel: SportsViewModel())
     }
 }
-
-
-/*var body: some View {
- ZStack {
-     NavigationView {
-         VStack(alignment: .center, spacing: 10) {
-             SportsView(viewModel: viewModel)
-             Spacer()
-             Text(viewModel.selectedSport?.title.uppercased() ?? "-")
-             Spacer()
-         } //: VStack
-         .navigationTitle(navigationViewTitle)
-         .navigationBarTitleDisplayMode(.inline)
-     } //: NAVIGATION
- } //: ZStack
-}*/
